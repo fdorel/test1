a7fc15275d7d9d65d29c0214815b2e03e5448a3d
Dorel Furdui <dorelfurdui@gmail.com>
2020-05-14 18:50:17 -0400

    Add files via upload

---
+++ javascript-modal/js/modal.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.buttonModalOpen').forEach(function (element) {
+    element.onclick = oPen;
+});
+
+document.querySelectorAll('.buttonModalClose').forEach(function (element) {
+    element.onclick = closePen;
+});
+
+function oPen() {
+    let modOpen = this.dataset.winopen;
+    document.querySelector(modOpen).classList.remove('hide');
+    document.onkeydown = function (event) {
+        if (event.keyCode == 27) closePen();
+    }
+}
+
+function closePen() {
+    document.querySelectorAll('.modalWrap').forEach(function (element) {
+        element.classList.add('hide');
+    });
+    document.onkeydown = null;
+}
