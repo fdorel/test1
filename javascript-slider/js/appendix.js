a6687686723caf01d7fa0c854d8c5062164219a1
Dorel Furdui <dorelfurdui@gmail.com>
2020-05-14 18:51:18 -0400

    Add files via upload

---
+++ javascript-slider/js/appendix.js
@@ -0,0 +1,7 @@
+document.querySelector('.appendix').onclick = function() {
+    document.querySelector('.sliderForm').style.marginLeft = '-350px';
+}
+
+document.querySelector('.appendixGoBack').onclick = function() {
+    document.querySelector('.sliderForm').style.marginLeft = '0px';
+}
